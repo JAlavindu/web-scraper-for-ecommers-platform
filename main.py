a3d0409cdf0dfedd31cdde5911020083a11e02e0
lavindu perera <lavi156perera@gmail.com>
2025-11-16 19:59:08 +0530

    updated file strucuture

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-import signup
+
+
+import accessing_daraz
+import signup_with_google
+
+
+ANTI_API_KEY = "2a0d3dd4c7e7c4851b796dc302daa8c1"
 
 if __name__ == "__main__":
-    signup()
+    accessing_daraz()
+    signup_with_google()
 
 
 
