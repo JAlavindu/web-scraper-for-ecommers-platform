f16d81fdb21588627dd900b9e20c7a49d182e4a0
lavindu perera <lavi156perera@gmail.com>
2025-11-21 23:24:34 +0530

    added more functionalities to login

--- login_with_google.py
+++ login_with_google.py
@@ -6,3 +6,11 @@ def google_login(driver):
     print("logging in with Google account...")
     login_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[text()='Login']")))
     login_button.click()
+
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, "//div[contains(@class,'iweb-dialog')]")))
+
+    google_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//div[contains(@class,'index_module_buttonItem')]")
+    ))
+    google_button.click()
