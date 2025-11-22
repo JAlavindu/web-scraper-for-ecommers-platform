5a9e19f546f67684f3944517ed81fd15cadffa50
lavindu perera <lavi156perera@gmail.com>
2025-11-22 20:01:11 +0530

    changed open_dara.py to use undetected chrome driver

--- open_daraz.py
+++ open_daraz.py
@@ -1,9 +1,23 @@
-from selenium import webdriver
-
-
-driver = webdriver.Chrome()
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def daraz_open():
+    global driver
 
-    driver.get("https://www.daraz.lk/#?")
+    options = uc.ChromeOptions()
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("--no-first-run --no-service-autorun --password-store=basic")
+    options.add_argument("--disable-infobars")
+
+    driver = uc.Chrome(options=options)
     driver.maximize_window()
+    driver.get("https://www.daraz.lk/")
+
+    WebDriverWait(driver, 15).until(
+        EC.presence_of_element_located((By.TAG_NAME, "body"))
+    )
+
+    print("Daraz website opened successfully")
+
